4c0465a363819cb40f3c2d54ae30a7811d10d9ec
Sam Welsh <samwelsh@hotmail.co.uk>
2024-01-20 22:10:13 +0000

    Add gridTemplateRows.layout to Tailwind theme.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,11 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    gridTemplateRows: {
+      'layout': 'auto 1fr',
+    }
+  },
   plugins: [],
 };
 export default config;
