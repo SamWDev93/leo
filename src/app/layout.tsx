5e22385545c8b3c87bf13d8ecda1cc03f586301a
Sam Welsh <samwelsh@hotmail.co.uk>
2024-01-20 20:19:31 +0000

    Re-add Tailwind defaults.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="h-full">
+      <body className={`h-full ${inter.className}`}>{children}</body>
     </html>
   );
 }
