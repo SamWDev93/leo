60576d6e4b4c2063b94821dee887d6658182c8c8
Sam Welsh <samwelsh@hotmail.co.uk>
2024-01-21 22:05:17 +0000

    Begin to add data to gift-ideas page.

--- src/app/gift-ideas/page.tsx
+++ src/app/gift-ideas/page.tsx
@@ -1,5 +1,43 @@
-export default function Page() {
+import { Cell, Column, Row, Table, TableBody, TableHeader } from 'react-aria-components';
+import { PrismaClient } from "@prisma/client";
+import { getSession } from '@auth0/nextjs-auth0';
+
+export default async function Page() {
+  const session = await getSession();
+  const user = session?.user;
+  let giftIdeas = null;
+  
+  if (user) {
+    const prisma = new PrismaClient();
+    giftIdeas = await prisma.giftIdea.findMany();
+  }
+
+  if (user) {
+    return (
+      giftIdeas && giftIdeas.length > 0 &&
+        <Table
+          aria-label="Gift Ideas"
+          className='w-full'
+        >
+          <TableHeader className='text-left'>
+            <Column isRowHeader>Name</Column>
+            <Column>Price</Column>
+            <Column>Link</Column>
+          </TableHeader>
+          <TableBody>
+          {giftIdeas.map((idea, index) => (
+            <Row key={index}>
+              <Cell>{idea.name}</Cell>
+              <Cell>{idea.price.toString()}</Cell>
+              <Cell>{idea.link}</Cell>
+            </Row>
+          ))}
+          </TableBody>
+        </Table>
+    )
+  }
+  
   return (
-    <div>Gift ideas page</div>
+    <div>Get lost</div>
   );
 }
